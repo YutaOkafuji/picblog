c11dbaf03bd7627e70271b90c2387f1ec396ecdd
ofuji <yutanoxxxuta@gmail.com>
2019-03-23 01:48:26 +0900

    Various edit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,23 @@
 class BlogsController < ApplicationController
-  before_action :index
 
   def index
-    @blog = Blog.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    @blog = Blog.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
+  end
+
+  def create
+    Blog.create(title: blog_params[:title],image: blog_params[:image],column: blog_params[:column])
+  end
+
+  def destroy
+  end
+
+  private
+
+  def blog_params
+    params.permit(:title,:image,:column,:user_id)
   end
 
 end
